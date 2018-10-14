021adf77015574ede028d9765f26e66f8c60bd7f
RhythmGilhotra <33939888+RhythmGilhotra@users.noreply.github.com>
2018-10-14 16:32:50 +0530

    Update and rename Num_of_Duplicates to Num_of_Duplicates.py

--- Num_of_Duplicates.py
+++ Num_of_Duplicates.py
@@ -1,6 +1,8 @@
+"""
 Count the number of Duplicates
 Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than 
 once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+"""
 
 from collections import Counter
 def duplicate_count(text):
